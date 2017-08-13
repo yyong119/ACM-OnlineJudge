6506b5182a1bf24106c88330b7fa537097e3b457
yyong <1479773600@qq.com>
2017-08-13 09:38:41 +0800

    a+b

---
+++ 1000.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String [] args){
+		
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println(a + b);
+		input.close();
+		
+	}
+}
