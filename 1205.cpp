b516134235d9d12a1c29ae19f5eb450861ffbca7
yyong <1479773600@qq.com>
2017-09-06 15:10:44 +0800

    ackerman function

---
+++ 1205.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+ 
+using namespace std;
+ 
+int ack(int m,int n)
+{
+    if(m == 0)
+        return n+1;
+    else if(n == 0)
+        return ack(m-1,1);
+    else
+        return ack(m-1,ack(m,n-1));
+}
+ 
+int main()
+{
+    int m, n;
+	cin>>m>>n;
+    cout<<ack(m, n);
+	return 0;
+ 
+ 
+}
