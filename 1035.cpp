71983c1d869d2568b20547f920ec35de1caf59da
yyong <1479773600@qq.com>
2017-12-17 20:44:12 +0800

    ...

--- 1035.cpp
+++ 1035.cpp
@@ -1,45 +1,43 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 using namespace std;
-const int MAX_N = 20010;
-struct node {
-	int id, num;
-};
- 
-inline bool cmp(node x, node y) {
-	if (x.num == y.num) return x.id < y.id;
-	else return x.num > y.num;
+#define number first
+#define price second
+const int SIZE = 21000;
+int byNumberprice_to_modify[SIZE], stockNumber, commandNumber, numberToModify, priceToModify;
+pair<int, int> byRanking[SIZE];
+bool Cmp(const pair<int, int> & stock1, const pair<int, int> & stock2) {
+	if(stock1.price == stock2.price)
+		return (stock1.number < stock2.number);
+	return (stock1.price > stock2.price);
 }
- 
-int main() {
- 
+int main(){
 	ios::sync_with_stdio(false);
-	int n; cin >> n;
-	node a[MAX_N], b[MAX_N];
-	for (int i = 1; i <= n; ++i) {
-		cin >> a[i].num;
-		a[i].id = i;
-	}
-	int k; cin >> k;
-	while (k--) {
-		char op[5];
-		cin >> op;
-		if (op[0] == 'Q') {
-			int r, temp; cin >> r;
-			memcpy(b, a, sizeof(a));
-			sort(b + 1, b + n + 1, cmp);
-			while (r--) {
-				cin >> temp;
-				cout << b[temp].id << " ";
-			}
-			cout << endl;
+	cin >> stockNumber;
+	for(int i = 0; i < stockNumber; ++i)
+		cin >> byNumberprice_to_modify[i];
+	cin >> commandNumber;
+	char command;
+	for(int i = 0; i < commandNumber; ++i) {
+		cin >>command;
+		if(command == 'M') {
+			cin >> numberToModify>> priceToModify;
+			byNumberprice_to_modify[numberToModify - 1] = priceToModify;
 		}
 		else {
-				int p, m;
-				cin >> p >> m;
-				a[p].num = m;
+			for(int i = 0; i < stockNumber; ++i) {
+				byRanking[i].number = i;
+				byRanking[i].price = byNumberprice_to_modify[i];
+			}
+			sort(byRanking, byRanking + stockNumber, Cmp);
+			int request_number, number_to_cout;
+			cin >> request_number;
+			for(int i = 0; i < request_number; ++i) {
+				cin >> number_to_cout;
+				cout << byRanking[number_to_cout - 1].number + 1 << ' ';
+			}
+			cout << '\n';
 		}
 	}
-	return 0;
+	return 0
 }
