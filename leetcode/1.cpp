b9c57e0ef805793b067d4d945327e949221a14a4
yyong <1479773600@qq.com>
2017-12-19 18:59:24 +0800

    TwoSum

---
+++ leetcode/1.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        for(int i = 0; i < nums.size(); ++i) {
+            for(int j = i + 1; j < nums.size(); ++j) {
+                if(nums[i] + nums[j] == target) {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+            }
+        }
+    }
+};
