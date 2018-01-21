1c9aa3093a8a0c4c557909abd114f5c4d7feff9f
yyong <1479773600@qq.com>
2018-01-21 17:43:37 +0800

    状态压缩dp

--- 1086.cpp
+++ 1086.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 
 const int MAXN = 105;
@@ -8,51 +9,43 @@ const int MAXM = 15;
 const int MAXS = 65;
 
 char matrix[MAXN][MAXM];
-int dp[MAXN][MAXS][MAXS], vst[MAXS], num[MAXS], originState[MAXN];
-
-int getnum(int i) {
-
-    int ret = 0;
-    for  (; i; i &= i - 1, ++ret);
-    return ret;
-}
+int f[MAXN][MAXS][MAXS], vst[MAXS], num[MAXS], originState[MAXN];
 
 int main() {
 
 	int n, m; scanf("%d%d", &n, &m);
 	for (int i = 0; i < n; ++i) {
 		originState[i] = 0;
-		scanf("%s", matrix[i]);
+		scanf("%s", &matrix[i]);
 		for (int j = 0; j < m; ++j)
 			if (matrix[i][j] == 'P')
 				originState[i] += 1 << (m - j - 1);
 	}
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < MAXS; ++j)
-			for (int k = 0; k < MAXS; ++k)
-				dp[i][j][k] = -1;
 
+	memset(f, sizeof(f), 0);
 	int vnum = 0;
 	for (int i = 0; i < (1 << m); ++i)
 		if (!(i & (i << 1)) && !(i & (i << 2))) {
 			vst[vnum] = i;
-			num[vnum++] = getnum(i);
+			int tmp = 0, r = i;
+			for (; r; r &= r - 1, ++tmp);
+			num[vnum++] = tmp;
 		}
 
 	for (int i = 0; i < vnum; ++i)
 		if (!(vst[i] & (~originState[0])))
-			dp[0][i][0] = num[i];
-	//dp[i][j][k] = max{dp[i][j][k], dp[i-1[k][t]]+num[j]}
+			f[0][i][0] = num[i];
+
 	for (int i = 1; i < n; ++i)
-		for (int j = 0; j < vnum; ++j) {//第i行
+		for (int j = 0; j < vnum; ++j) { //第i行
 			if ((~originState[i]) & vst[j]) continue;
-			for (int k = 0; k < vnum; ++k) {//第i - 1行
-				if ((~originState[i-1]) & vst[k]) continue;
+			for (int k = 0; k < vnum; ++k) { //第i - 1行
+				if ((~originState[i - 1]) & vst[k]) continue;
 				if (vst[j] & vst[k]) continue;
-				for (int t = 0; t < vnum; ++t) {//第i - 2行
+				for (int t = 0; t < vnum; ++t) { //第i - 2行
 					if (vst[j] & vst[t]) continue;
-					if (-1 == dp[i-1][k][t]) continue;
-					dp[i][j][k] = max(dp[i][j][k], dp[i - 1][k][t] + num[j]);
+					if (f[i - 1][k][t] == -1) continue;
+					f[i][j][k] = max(f[i][j][k], f[i - 1][k][t] + num[j]);
 				}
 			}
 		}
@@ -60,7 +53,7 @@ int main() {
 	int ret = 0;
 	for (int i = 0; i < vnum; ++i)
 		for (int j = 0; j < vnum; ++j)
-			ret = max(dp[n - 1][i][j], ret);
+			ret = max(f[n - 1][i][j], ret);
 	printf("%d\n", ret);
 	return 0;
 }
