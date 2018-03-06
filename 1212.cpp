fff0487dd4d1a8cbe006379b0f8b1276f26edfa8
Yong Mao <yyong119@sjtu.edu.cn>
2018-03-06 14:44:45 +0800

    二叉树

--- 1212.cpp
+++ 1212.cpp
@@ -1,30 +1,32 @@
-#include <iostream>
+#include <cstdio>
 #include <queue>
- 
 using namespace std;
-struct treedata {
-	int l, r, pre, value;
-} tree[100001];
-int n, i, now, root;
-queue<int> que;
- 
+
+const int MAX_N = 100000;
+struct node_data {
+	int left_child, right_child, father, value;
+} node[MAX_N + 10];
+
 int main() {
- 
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cin >> tree[i].l >> tree[i].r >> tree[i].value;
-		tree[tree[i].l].pre = i; tree[tree[i].r].pre = i;
+
+	int n; scanf("%d", &n);
+	for (int i = 1; i <= n; ++i) {
+		int tmp_left, tmp_right, tmp_value; scanf("%d%d%d", &tmp_left, &tmp_right, &tmp_value);
+		node[i].value = tmp_value;
+		node[i].left_child = tmp_left; node[tmp_left].father = i;
+		node[i].right_child = tmp_right; node[tmp_right].father = i;
 	}
-	for (root = 1; root <= n; ++root) if (!tree[root].pre) break;
-	if (tree[root].l) que.push(tree[root].l);
-	if (tree[root].r) que.push(tree[root].r);
-	cout << tree[root].value;
+
+	int root = 1;
+	while (node[root].father) root = node[root].father;
+	queue<int> que; que.push(root);
+
 	while (!que.empty()) {
-		now = que.front();
-		que.pop();
-		cout << " " << tree[now].value;
-		if (tree[now].l) que.push(tree[now].l);
-		if (tree[now].r) que.push(tree[now].r);
+		int tmp = que.front(); que.pop();
+		printf("%d ", node[tmp].value);
+		if (node[tmp].left_child) que.push(node[tmp].left_child);
+		if (node[tmp].right_child) que.push(node[tmp].right_child);
 	}
+
 	return 0;
 }
