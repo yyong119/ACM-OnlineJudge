e45a6eeddbb49ed79e7467329cd545a2f51b650b
Yong Mao <yyong119@sjtu.edu.cn>
2019-06-24 12:41:39 +0800

    segment tree
    change from 1e5 to 1e6

--- 1371.cpp
+++ 1371.cpp
@@ -5,40 +5,40 @@ int tree[MAX_N << 2];
  
 void update(int t, int l, int r, int point) {
  
-	++tree[t];
-	if (l == r) return;
-	int mid = (l + r) >> 1;
-	if (point <= mid)
+    ++tree[t];
+    if (l == r) return;
+    int mid = (l + r) >> 1;
+    if (point <= mid)
         update(t << 1, l, mid, point);
-	else
+    else
         update(t << 1 | 1, mid + 1, r, point);
 }
  
 int query(int t, int l, int r, int ql, int qr) {
  
-	if (l == ql && r == qr) return tree[t];
-	int mid = (l + r) >> 1;
-	if (qr <= mid)
+    if (l == ql && r == qr) return tree[t];
+    int mid = (l + r) >> 1;
+    if (qr <= mid)
         return query(t << 1, l, mid, ql, qr);
-	else if (ql > mid)
+    else if (ql > mid)
         return query(t << 1 | 1, mid + 1, r, ql, qr);
-	else
+    else
         return query(t << 1, l, mid, ql, mid) + query(t << 1 | 1, mid + 1, r, mid + 1, qr);
 }
  
 int main() {
  
-	int n, m;
-	scanf("%d%d", &n, &m);
-	for (int i = 0; i < n; ++i) {
-		int tmp;
-		scanf("%d", &tmp);
-		update(1, 1, n, tmp);
-	}
-	for (int i = 0; i < m; ++i) {
-		int tmpl, tmpr;
-		scanf("%d%d", &tmpl, &tmpr);
-		printf("%d\n", query(1, 1, n, tmpl, tmpr));
-	}
-	return 0;
+    int n, m;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; ++i) {
+        int tmp;
+        scanf("%d", &tmp);
+        update(1, 1, n, tmp);
+    }
+    for (int i = 0; i < m; ++i) {
+        int tmpl, tmpr;
+        scanf("%d%d", &tmpl, &tmpr);
+        printf("%d\n", query(1, 1, n, tmpl, tmpr));
+    }
+    return 0;
 }
