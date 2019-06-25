77777ed5db01219f1417a20b9cb44f7dbdf444c8
yyong119 <yyyuyong119@gmail.com>
2019-06-25 16:01:34 +0800

    dp

--- 1069.cpp
+++ 1069.cpp
@@ -1,32 +1,37 @@
 #include <cstdio>
 #include <cstring>
-#include <iostream>
-#include <cmath>
-#include <algorithm>
-#include <cstdlib>
+#define MAX_N 105
+#define MAX_M 100005
 using namespace std;  
   
 int n, m;
-int w[105], c[105], sum[100005], dp[100005];
+int w[MAX_N], c[MAX_N], sum[MAX_M], f[MAX_M];
   
 int main() {
 
     while (~scanf("%d%d", &n, &m)) {
-        if (n == 0 && m == 0) break;
-        for (int i = 0; i < n; ++i) scanf("%d", &w[i]);
-        for (int i = 0; i < n; ++i) scanf("%d", &c[i]);
-        memset(dp, 0, sizeof(dp));
-        dp[0] = 1;
-        int ans = 0;
+
+        if (n == 0 && m == 0)
+            break;
+        for (int i = 0; i < n; ++i)
+            scanf("%d", &w[i]);
+        for (int i = 0; i < n; ++i)
+            scanf("%d", &c[i]);
+
+        memset(f, 0, sizeof(f));
+        f[0] = 1;
         for (int i = 0; i < n; ++i) {
             memset(sum,0,sizeof(sum));
             for (int j = w[i]; j <= m; ++j)
-                if(!dp[j] && dp[j - w[i]] && sum[j - w[i]] < c[i]) {
-                    dp[j] = 1;
+                if(!f[j] && f[j - w[i]] && sum[j - w[i]] < c[i]) {
+                    f[j] = 1;
                     sum[j] = sum[j - w[i]] + 1;
-                    ++ans;
                 }
         }
+        int ans = 0;
+        for (int i = 1; i <= m; ++i)
+            if (f[i])
+                ++ans;
         printf("%d\n", ans);
     }
     return 0;
