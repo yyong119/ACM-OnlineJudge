67a3e2a514037d14a7afbd08c77ffad753974e2b
yyong119 <yyyuyong119@gmail.com>
2019-06-26 23:00:30 +0800

    the most efficient reading method of cpp

--- 0_the_best_reading_method.cpp
+++ 0_the_best_reading_method.cpp
@@ -4,14 +4,19 @@ using namespace std;
 int a, b;
 
 inline int read() {
+    
     int p, data = 0;
     char ch = 0;
-    while ((ch != '-') && ch < '0' || ch > '9') ch = getchar();
+    while ((ch != '-') && ch < '0' || ch > '9')
+        ch = getchar();
     if (ch=='-') {
         p = -1;
         ch = getchar();
-    } else p = 1;
-    while (ch >= '0' && ch <= '9') data = data * 10 + ch - '0', ch = getchar();
+    }
+    else
+        p = 1;
+    while (ch >= '0' && ch <= '9')
+        data = data * 10 + ch - '0', ch = getchar();
     return data * p;
 }
 
