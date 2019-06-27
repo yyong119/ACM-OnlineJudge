9d7d30c66e9da4dcd8245cff41ad24d0d3f9a5a2
yyong119 <yyyuyong119@gmail.com>
2019-06-27 11:11:19 +0800

    tree dp

--- 1554.cpp
+++ 1554.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
+#include <cstdio>
 #include <queue>
 #include <stack>
 #include <cstring>
 #include <algorithm>
 using namespace std;
- 
-const int MAX_N = 800001;
-const long INF = 1 << 30;
+#define MAX_N 800010
+#define INF 0x7fffffff
+
 struct node {
 	int fpoint, tpoint, cost;
 	node(int p, int q, int cc) {
@@ -17,75 +17,81 @@ struct node {
 	};
 }tree[MAX_N << 1];
  
-int start[MAX_N], pre[MAX_N];
+int start_index[MAX_N], par[MAX_N];
 long f[MAX_N];
 bool visited[MAX_N];
  
 inline bool cmp(node p, node q) {
 	return p.fpoint != q.fpoint ? p.fpoint < q.fpoint : p.tpoint < q.tpoint;
 }
- 
+
 int main() {
- 
-	ios::sync_with_stdio(false);
+
 	int n, root;
-	cin >> n >> root;
+	scanf("%d%d", &n, &root);
+
 	for (int i = 1; i < n; ++i) {
- 
 		int temp1, temp2, co;
-		cin >> temp1 >> temp2 >> co;
+		scanf("%d%d%d", &temp1, &temp2, &co);
 		tree[i] = node(temp1, temp2, co);
 		tree[i + n - 1] = node(temp2, temp1, co);
 	}
-	tree[2 * n - 1] = node(root, 0, INF);
- 
-	sort(tree + 1, tree + 2 * n, cmp);
-	for (int i = 2 * n - 1; i > 0; --i) start[tree[i].fpoint] = i;
-	start[n + 1] = 2 * n;
- 
+	tree[(n << 1) - 1] = node(root, 0, INF);
+
+	// In ascending order
+	sort(tree + 1, tree + (n << 1), cmp);
+	// Mark the start_index index of each node
+	for (int i = (n << 1) - 1; i > 0; --i)
+		start_index[tree[i].fpoint] = i;
+	start_index[n + 1] = n << 1;
+
 	visited[0] = true;
- 
-	queue<int> que; que.push(root);
+	queue<int> que;
+	que.push(root);
 	while (!que.empty()) {
- 
-		int now = que.front(); visited[now] = true;
-		for (int i = start[now]; i < start[now + 1]; ++i) {
+		int cur = que.front(); que.pop();
+		visited[cur] = true;
+		// Indexes of cur
+		for (int i = start_index[cur]; i < start_index[cur + 1]; ++i) {
 			int next = tree[i].tpoint;
-			if (visited[next]) pre[now] = next;
-			else {
-				pre[next] = now;
+			if (visited[next]) // next is parent
+				par[cur] = next;
+			else { // next is child
+				par[next] = cur;
 				que.push(next);
 			}
 		}
-		que.pop();
 	}
- 
-	memset(visited, false, sizeof(visited)); visited[0] = true;
- 
+
+	memset(visited, false, sizeof(visited));
+	visited[0] = true;
+
 	stack<int> sta;
 	sta.push(root); que.push(root);
 	while (!que.empty()) {
- 
-		int now = que.front(); visited[now] = true;
-		for (int i = start[now]; i < start[now + 1]; ++i) {
+
+		int cur = que.front(); que.pop();
+		visited[cur] = true;
+		// Indexes of node cur
+		for (int i = start_index[cur]; i < start_index[cur + 1]; ++i)
 			if (!visited[tree[i].tpoint]) {
 				que.push(tree[i].tpoint);
 				sta.push(tree[i].tpoint);
 				visited[tree[i].tpoint] = true;
 			}
-		}
-		que.pop();
 	}
- 
+
 	while (!sta.empty()) {
-		int now = sta.top(); sta.pop();
-		if (start[now + 1] - start[now] == 1)
-			f[now] = tree[start[now]].cost;
+		int cur = sta.top(); sta.pop();
+		// Leaf node
+		if (start_index[cur + 1] - start_index[cur] == 1)
+			f[cur] = tree[start_index[cur]].cost;
 		else {
-			for (int i = start[now]; i < start[now + 1]; ++i)
-				f[now] += f[tree[i].tpoint] < tree[i].cost ? f[tree[i].tpoint] : tree[i].cost;
+			for (int i = start_index[cur]; i < start_index[cur + 1]; ++i)
+				f[cur] += f[tree[i].tpoint] < tree[i].cost ? f[tree[i].tpoint] : tree[i].cost;
 		}
 	}
-	cout << f[root] << endl;
+
+	printf("%d\n", f[root]);
 	return 0;
 }
