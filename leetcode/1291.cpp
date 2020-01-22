84561d113790c4469f1845be1a0ffdadd16a068f
yyong119 <yyong119@sjtu.edu.cn>
2020-01-22 16:51:16 +0800

    ...

---
+++ leetcode/1291.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> ans;
+        for (int i = 1; i <= 9; ++i) {
+            int num = i;
+            for (int j = i + 1; j <= 9; ++j) {
+                num = num * 10 + j;
+                if (num >= low && num <= high) {
+                    ans.push_back(num);
+                }
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
