95a02c71c23b6f2c7a9848086bac3886ec5b9844
Yong Mao <yyong119@sjtu.edu.cn>
2020-01-27 09:33:02 +0800

    ...

---
+++ leetcode/700.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (!root) return nullptr;
+        if (root->val == val) return root;
+        return root->val < val ? searchBST(root->right, val) : searchBST(root->left, val);
+    }
+};
