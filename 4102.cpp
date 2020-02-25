45a6085738a86808ea9576665a4c29d87682a095
yyong119 <yyong119@sjtu.edu.cn>
2020-02-25 09:41:10 +0800

    单调队列

--- 4102.cpp
+++ 4102.cpp
@@ -1,35 +1,31 @@
-#include<iostream>
 #include<cstdio>
-#include<cstring>
-#include<cmath>
-#include<algorithm>
-#define GET (ch>='0'&&ch<='9')
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define MAX_N 3000010
 using namespace std;
-
-const int MAXN = 3000010;
-int n, k, ans, h1, t1, h2, t2;
-int a[MAXN], q1[MAXN],q2[MAXN];
- 
-void in(int &x) {
-    char ch = getchar();
-    x = 0;
-    while (!GET) ch = getchar();
-    while (GET) x = x * 10 + ch - '0', ch = getchar();
+int n, k, ans, h1, t1 = -1, h2, t2 = -1, tmp;
+int a[MAX_N], q1[MAX_N],q2[MAX_N];
+inline int read() {
+    char ch = getchar(); int res = 0;
+    while (ch < '0' || ch > '9') ch = getchar();
+    while (ch >= '0' && ch <= '9')
+        res = (res << 3) + (res << 1) + (ch ^ 48), ch = getchar();
+    return res;
 }
- 
 int main() {
- 
-    in(k);in(n);
-    int tmp=1;
-    for (int i = 1; i <= n; ++i) {
-        in(a[i]);
-        while (h1 != t1 && a[i] >= a[q1[t1 - 1]]) --t1;
-        while (h2 != t2 && a[i] <= a[q2[t2 - 1]]) --t2;
-        q1[t1++] = i; q2[t2++] = i;
+    k = read(); n = read();
+    for (register int i = 0; i < n; ++i) {
+        a[i] = read();
+        // q1 interval maximum
+        while (h1 <= t1 && a[i] >= a[q1[t1]]) --t1;
+        // q2 interval minimum
+        while (h2 <= t2 && a[i] <= a[q2[t2]]) --t2;
+        // enqueue
+        q1[++t1] = i; q2[++t2] = i;
         while (a[q1[h1]] - a[q2[h2]] > k)
-            if (q1[h1] < q2[h2]) tmp = q1[h1] + 1, ++h1;
-            else tmp = q2[h2] + 1, ++h2;
-        ans = max(ans, i - tmp + 1);
+            tmp = q1[h1] < q2[h2] ? q1[h1++] + 1 : q2[h2++] + 1;
+            // if (q1[h1] < q2[h2]) tmp = q1[h1++] + 1;
+            // else tmp = q2[h2++] + 1;
+        ans = max(ans, i - tmp);
     }
-    printf("%d\n", ans);
+    printf("%d", ans + 1);
 }
